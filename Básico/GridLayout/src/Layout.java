9199789ed73fd07806e0b7703cc557024ac197e3
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-11 11:22:01 -0300

    GridLayout

---
+++ Básico/GridLayout/src/Layout.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Layout extends JFrame {
+
+    JButton jb = new JButton("Oi");
+    JButton jb2 = new JButton("Hola");
+    JButton jb3 = new JButton("Hello");
+    JButton jb4 = new JButton("Bonjour");
+
+    public Layout(){
+
+        setLayout(new GridLayout(2,2));
+        add(jb);
+        add(jb2);
+        add(jb3);
+        add(jb4);
+
+        setTitle("Título");
+        setSize(300, 380);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Layout();
+    }
+}
