28a9c2c9e2d82e9a02b531884fcad9044f1279aa
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-11 23:21:43 -0300

    array aprimorado

---
+++ Básico/ArrayList/src/ArrayAprimorado.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayAprimorado {
+    public static void main(String[] args) {
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Luccas");
+        nomes.add("Raquel");
+
+        System.out.println(nomes.toString());
+    }
+}
