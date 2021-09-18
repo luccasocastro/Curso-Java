41d2cbff3a5ae1210ba32bb141647600af3e3ce3
Luccas Castro <emaildocastro.adm01@gmail.com>
2021-09-18 14:57:02 -0300

    this is a test

---
+++ POO/Exercício001/src/teste/testando/Teste.java
@@ -0,0 +1,7 @@
+package teste.testando;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Isto é um teste, relaxa e goza q a vida é rosa...");
+    }
+}
